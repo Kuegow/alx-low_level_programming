e1dd391bb965d047f5995794bb4cf5eed8ee365e
Olushoto <olushototaiwo@gmail.com>
2023-08-10 20:56:17 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char sentence[] = "Programming is like building a multilingual puzzle";
+
+	puts(sentence);
+
+	/* using puts function to print the sentence */
+	return (0);
+}
