90d7bc3d6e99cebddb6fda43d2d49dd506072825
Olushoto <olushototaiwo@gmail.com>
2023-08-16 15:41:03 -0700

    print absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - print absolute number
+ * @n: number to be checked for absolute value
+ *
+ * Return: n if absolute, otheriwse -n
+ */
+int _abs(int n)
+{
+		if (n >= 0)
+		{
+			return (n);
+		}
+		return (-n);
+}
