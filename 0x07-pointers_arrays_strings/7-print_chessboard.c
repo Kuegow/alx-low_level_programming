ec3a99cd89b4ff866a85b208d7664abdddcb412b
Olushoto <olushototaiwo@gmail.com>
2023-08-28 17:44:08 -0700

    print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - function that prints chessboard.
+ * @a: array
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+	int j = 0;
+
+	while (i < 8)
+	{
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
