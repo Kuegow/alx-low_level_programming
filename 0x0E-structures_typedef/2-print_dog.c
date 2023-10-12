c500a659726ab0a027f4e0b36adf6ee87946589e
Taiwo <olushototaiwo@gmail.com>
2023-10-11 17:40:25 -0700

    update to <2-print_dog>

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,11 +13,13 @@ void print_dog(struct dog *d)
 		return;
 
 	if ((*d).name == NULL)
-		printf("nil");
-	if ((*d).age == 0)
-		printf("nil");
+	{
+		(*d).name = "(nil)";
+	}
 	if ((*d).owner == NULL)
-		printf("nil");
+	{
+		(*d).owner = "(nil)";
+	}
 
 	printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
 }
