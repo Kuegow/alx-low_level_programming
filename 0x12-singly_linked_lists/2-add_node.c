874a40e7e277790e3d99707eaf2545d9d0761388
Taiwo <olushototaiwo@gmail.com>
2023-10-21 05:04:06 -0700

    added return NULL if malloc failed

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,6 +14,9 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 
+	if (new_node == NULL)
+		return (NULL);
+
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 
