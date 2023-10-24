46516ed62118322f1772dd475009c850ac6d25cd
Taiwo <olushototaiwo@gmail.com>
2023-10-24 03:28:17 -0700

    added condition for if strdup fails

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -19,6 +19,13 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 
 	(*new_node).str = strdup(str);
+
+	if ((*new_node).str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
 	(*new_node).len = strlen(str);
 	(*new_node).next = NULL;
 
