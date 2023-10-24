68aa56a448cf61364d31bc8ee2e9a858bdc64d62
Taiwo <olushototaiwo@gmail.com>
2023-10-24 06:38:38 -0700

    function that prints elements of a singly linked list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "lists.h"
+
+/**
+ * print_listint - prints elements of a singly linked list
+ * @h: - head node address
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *temp = h;
+	int count = 0;
+
+	while (temp != NULL)
+	{
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		count++;
+	}
+	return (count);
+}
