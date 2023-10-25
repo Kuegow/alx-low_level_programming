652aac971e9cb4516054713bb44809e08bb934da
Taiwo <olushototaiwo@gmail.com>
2023-10-24 20:51:14 -0700

    brush up code

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,14 +11,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-
 	if (*head == NULL)
 		return;
 
-	while (*head != NULL)
+	while (*head)
 	{
-		temp = *head;
-		*head = (*head)->next;
-		free(temp);
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
+	*head = NULL;
 }
